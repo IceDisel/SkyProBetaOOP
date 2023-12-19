b9d987c020ed02fd6db92b7e6fc2cda1e0e6cc63
Vladimir Titov <tvv-vg@yandex.ru>
2023-12-19 19:46:25 +0300

    add class Product

--- models/Product.py
+++ models/Product.py
@@ -1,2 +1,15 @@
+from models.Category import Category
+
+
 class Product:
-    pass
+    name_product: str
+    description_product: str
+    price: float
+    quantity_in_stock: str
+
+    def __init__(self, name_product, description_product, price, quantity_in_stock):
+        self.name_product = name_product
+        self.description_product = description_product
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
+        Category.total_unique_products += 1
