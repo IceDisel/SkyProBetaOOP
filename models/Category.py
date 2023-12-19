3fdf3db84873a858f1985514edc5ca397ec224df
Vladimir Titov <tvv-vg@yandex.ru>
2023-12-19 19:46:38 +0300

    add class Category

--- models/Category.py
+++ models/Category.py
@@ -1,2 +1,13 @@
 class Category:
-    pass
+    name_category: str
+    description_category: str
+    products: list
+
+    total_categories = 0
+    total_unique_products = 0
+
+    def __init__(self, name_category, description_category):
+        self.name_category = name_category
+        self.description_category = description_category
+        self.products = []
+        Category.total_categories += 1
