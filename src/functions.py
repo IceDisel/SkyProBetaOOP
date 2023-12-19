0865634896d84286040b9ad6321386e98b301eb7
Vladimir Titov <tvv-vg@yandex.ru>
2023-12-19 21:15:23 +0300

    add functions.py and def read_json_file

---
+++ src/functions.py
@@ -0,0 +1,12 @@
+import json
+
+
+def read_json_file(path: str) -> list[dict]:
+    """
+    Функция чтения JSON файла
+    :param path: Путь до файла
+    :return: Список словарей с данными
+    """
+    with open(path, encoding='UTF-8') as json_file:
+        json_list = json.load(json_file)
+    return json_list
