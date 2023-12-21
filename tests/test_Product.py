939607e3799d68a75656e8a6eb7063a562887b75
Vladimir Titov <tvv-vg@yandex.ru>
2023-12-21 20:29:25 +0300

    add test_Product.py

---
+++ tests/test_Product.py
@@ -0,0 +1,19 @@
+import pytest
+
+from models.Category import Category
+from models.Product import Product
+
+
+@pytest.fixture
+def product_notebook():
+    return Product("Ноутбук Acer Nitro 5", "Игровой ноутбук с экраном 144Hz",
+                   150_000, 3)
+
+
+def test_init(product_notebook):
+    assert product_notebook.name_product == "Ноутбук Acer Nitro 5"
+    assert product_notebook.description_product == "Игровой ноутбук с экраном 144Hz"
+    assert product_notebook.price == 150_000
+    assert product_notebook.quantity_in_stock == 3
+
+    assert Category.total_unique_products == 1
