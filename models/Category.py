4d00b5444d61d377ffa3a9a76b1f06574556d9f8
Vladimir Titov <tvv-vg@yandex.ru>
2023-12-24 21:06:25 +0300

    add getter, setter

--- models/Category.py
+++ models/Category.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Category:
     """
     Класс, представляющий категорию продуктов.
@@ -9,7 +12,7 @@ class Category:
     total_unique_products = 0
     """Общее количество уникальных продуктов."""
 
-    def __init__(self, name_category: str, description_category: str) -> None:
+    def __init__(self, name_category: str, description_category: str, product: Any = None) -> None:
         """
         Инициализация объекта класса Category.
         :param name_category: Название категории.
@@ -17,5 +20,18 @@ class Category:
         """
         self.name_category = name_category
         self.description_category = description_category
-        self.products = []
+        self.__products = []
+        self.__products.append(product)
         Category.total_categories += 1
+
+    @property
+    def products_list(self):
+        tmp_list = []
+        for product in self.__products:
+            if product:
+                tmp_list.append(f"{product.name_product}, {product.price} руб. Остаток: {product.quantity_in_stock}")
+        return tmp_list
+
+    @products_list.setter
+    def products_list(self, product):
+        self.__products.append(product)
